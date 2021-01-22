7e28225e6b6eb0848ededb33a00a91c17d72c8d9
zyq1137533407 <1137533407@qq.com>
2021-01-22 23:08:37 +0800

    增加ImlicitDemo

---
+++ ImlicitDemo/AuthorizationServerDemo/Config.cs
@@ -0,0 +1,93 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthorizationServerDemo
+{
+    public class Config
+    {
+        // API权限范围作用域
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("orderApi","Order Api")
+            };
+        }
+
+        // 模拟备案用户
+        public static List<TestUser> GetTestUsers()
+        {
+            // 模拟两个用户
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                     SubjectId="1",
+                     Username="Zoe", // 用户名
+                     Password="123456",// 密码
+                      Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "Zoe"),
+                            new Claim(JwtClaimTypes.GivenName, "Zoe11"),
+                            new Claim(JwtClaimTypes.FamilyName, "ZZZ"),
+                            new Claim(JwtClaimTypes.Email, "Zoe@email.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.WebSite, "http://alice.com")
+                    }
+
+                },
+                new TestUser
+                {
+                     SubjectId="2",
+                     Username="ZoeCode",// 用户名
+                     Password="666999"// 用户名
+                }
+            };
+        }
+
+        // 模拟备案客户端，不是任意一个应用都能到认证授权中心获取Token
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                   // 客户端的唯一标识
+                   ClientId="client", 
+                   // 客户端认证密码
+                   ClientSecrets =
+                    {
+                        new Secret("ordersecret".Sha256())
+                    },
+                   // 指定授权模式，这里指定为客户端凭据模式
+                   AllowedGrantTypes = GrantTypes.ClientCredentials,
+                   // 指定客户端获取的Token能访问到的作用域
+                   AllowedScopes={ "orderApi" }
+                },
+                new Client
+                {
+                    // 客户端的唯一标识
+                    ClientId="WinForm",
+                    // 客户端认证密码
+                    ClientSecrets=
+                    {
+                        new Secret("winformsecret".Sha256())
+                    },
+                   // 指定授权模式，这里指定为客户端凭据模式
+                   AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                   // 如果要获取refreshtoken，将AllowOfflineAccess要设置为true
+                   AllowOfflineAccess=true,
+                   // 指定客户端获取的Token能访问到的作用域
+                   AllowedScopes={ "orderApi" }
+
+                }
+            };
+        }
+    }
+}
---
+++ ImlicitDemo/AuthorizationServerDemo/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AuthorizationServerDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            // 注入对应的IdentityServer相关组件， 然后加载模拟数据
+            var builder = services.AddIdentityServer()
+                .AddInMemoryApiScopes(Config.GetApiScopes())
+                .AddInMemoryClients(Config.GetClients())
+                // 模拟备案用户，即资源拥有者
+                .AddTestUsers(Config.GetTestUsers());
+
+            // 指定Token签名和验证的秘钥方式， 开发测试使用临时秘钥保存在本地；
+            // 生产用AddSigingCredential, 这里后续会说到
+            builder.AddDeveloperSigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            // 开启中间件
+            app.UseIdentityServer();
+
+           
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
