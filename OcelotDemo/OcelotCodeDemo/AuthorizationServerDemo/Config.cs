7403e5f42849232e2ad1b12cc3927cda1e88139c
zyq1137533407 <1137533407@qq.com>
2021-04-28 00:05:15 +0800

    增加Ids4的授权服务项目

---
+++ OcelotDemo/OcelotCodeDemo/AuthorizationServerDemo/Config.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationServerDemo
+{
+    public class Config
+    {
+        // API权限范围作用域
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("orderApi","Order Api")
+            };
+        }
+        // 模拟备案客户端，不是任意一个应用都能到认证授权中心获取Token
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                   // 客户端的唯一标识
+                   ClientId="client", 
+                   // 客户端认证密码
+                   ClientSecrets =
+                    {
+                        new Secret("ordersecret".Sha256())
+                    },
+                   // 指定授权模式，这里指定为客户端凭据模式
+                   AllowedGrantTypes = GrantTypes.ClientCredentials,
+                   // 指定客户端获取的Token能访问到的作用域
+                   AllowedScopes={ "orderApi" }
+                }
+            };
+        }
+    }
+}
---
+++ OcelotDemo/OcelotCodeDemo/OcelotCodeDemo/MyCache.cs
@@ -0,0 +1,47 @@
+﻿using Ocelot.Cache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OcelotCodeDemo
+{
+    public class MyCache : IOcelotCache<CachedResponse>
+    {
+        private class MyCacheModel
+        {
+            public string Region { get; set; }
+            public TimeSpan Ttl { get; set; }
+            public CachedResponse CachedResponse { get; set; }
+        }
+        private static Dictionary<string, MyCacheModel> myCacheDic = new Dictionary<string, MyCacheModel>();
+        void IOcelotCache<CachedResponse>.Add(string key, CachedResponse value, TimeSpan ttl, string region)
+        {
+
+            myCacheDic[key] = new MyCacheModel { CachedResponse = value, Ttl = ttl, Region = region };
+            Console.WriteLine("自定义缓存，想咋弄都行~~~+Add");
+
+        }
+
+        void IOcelotCache<CachedResponse>.AddAndDelete(string key, CachedResponse value, TimeSpan ttl, string region)
+        {
+            myCacheDic[key] = new MyCacheModel { CachedResponse = value, Ttl = ttl, Region = region };
+            Console.WriteLine("自定义缓存，想咋弄都行~~~+AddAndDelete");
+        }
+
+        void IOcelotCache<CachedResponse>.ClearRegion(string region)
+        {
+            Console.WriteLine("自定义缓存，想咋弄都行~~~+ClearRegion");
+        }
+
+        CachedResponse IOcelotCache<CachedResponse>.Get(string key, string region)
+        {
+            Console.WriteLine("自定义缓存，想咋弄都行~~~+Get");
+            if(!myCacheDic.ContainsKey(key))
+            {
+                return null;
+            }
+            return myCacheDic[key].CachedResponse;
+        }
+    }
+}
